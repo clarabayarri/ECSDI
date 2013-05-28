755824e79603b997c9edc6b963ed95990ad66cf3
Clara <nayami.ruka@gmail.com>
2013-05-28 18:22:44 +0200

    Funcions Pacient

---
+++ src/java/ecsdi/ECSDIMain.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecsdi;
+
+/**
+ *
+ * @author clara.bayarri
+ */
+public class ECSDIMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
